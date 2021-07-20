7f88b948d2bf7857b3d49de3094256c463a3ace2
Advaitaaa <73975899+Advaitaaa@users.noreply.github.com>
2021-07-20 12:26:58 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -8,4 +8,29 @@ function setup()
     video=createCapture(VIDEO);
     video.size(300,300);
     video.hide();
+    classifier=ml5.imageClassifier("model.json" , modelLoaded) ;
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+}
+
+function draw()
+{
+    image(video,0,0,300,300);
+    classifier.classify(video,gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error) {
+        console.error(error);
+    }
+else 
+{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML=results[0].label
+    documen.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(2)
+}
 }
