4a14b6ec8171c3110b0ce2096562dab364a88ef8
Advaitaaa <73975899+Advaitaaa@users.noreply.github.com>
2021-08-02 09:16:42 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,29 @@
-function preload() {
-}
-
 function setup() 
 {
-    canvas=createCanvas(300,300);
+    canvas = createCanvas(300,300);
     canvas.center();
-    video=createCapture(VIDEO);
-    video.size(300,300);
+    video = createCanvas(VIDEO);
     video.hide();
-    classifier=ml5.imageClassifier("model.json" , modelLoaded) ;
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl958zE/model.json',modelLoaded);
 }
 
-function modelLoaded()
-{
-    console.log("Model Loaded");
+function modelLoaded() {
+    console.log('Model Loaded');
 }
 
 function draw()
 {
-    image(video,0,0,300,300);
-    classifier.classify(video,gotResult);
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video,gotResults);
 }
 
-function gotResult(error, results)
+function gotResults(error, results) 
 {
     if (error) {
         console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("results_object_name").innerHTML = results = results[0].label;
+        document.getElementById("results_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
     }
-else 
-{
-    console.log(results);
-    document.getElementById("result_object_name").innerHTML=results[0].label
-    documen.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(2)
-}
 }
